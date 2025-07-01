263e2b801fb6aff03520b0f6e27d958115eaa974
christian <christianphilpot06@gmail.com>
2025-07-01 13:44:41 -0400

    Initial commit

---
+++ MainWindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by christian on 7/1/25.
+//
+
+#include "MainWindow.h"
---
+++ MainWindow.h
@@ -0,0 +1,16 @@
+//
+// Created by christian on 7/1/25.
+//
+
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+
+
+class MainWindow {
+
+};
+
+
+
+#endif //MAINWINDOW_H
